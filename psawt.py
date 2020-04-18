d14140d7008b8ec8823dee517e56eacb14b03ae9
SamSpet <52170426+SamSpet@users.noreply.github.com>
2020-04-18 15:27:47 -0400

    psawt.py now produces our usual CSV

--- psawt.py
+++ psawt.py
@@ -1,34 +1,29 @@
 from psaw import PushshiftAPI
-import datetime as dt
-import xlsxwriter
-
-#name the file what you want
-workbook = xlsxwriter.Workbook('hello.xlsx')
-worksheet = workbook.add_worksheet()
+from datetime import datetime
+from pandas import DataFrame as df
 
 api = PushshiftAPI()
 #choose the date you want to start from yyyy-mm-dd
-start_epoch=int(dt.datetime(2020, 4, 10).timestamp())
+start_epoch=int(datetime(2020, 4, 10).timestamp())
 
 #use the .txt file with the phrases you want
 words = open("finance_words.txt","r").read().splitlines()
 
-col = 0
+#DataFrame to write the CSV
+
+reddit_data = {}
 #create one column at a time for each search word
 for text in words:
-    col = col + 1
     print(text)
     #choose the subreddit you want to search
     gen = api.search_comments(after = start_epoch, q = text, subreddit='worldnews', aggs = 'created_utc', frequency = 'day', size = '0')
+    
+    for item_list in gen:        
+        for item in item_list['created_utc']:
+            date = datetime.utcfromtimestamp(item['key']).strftime('%Y-%m-%d')
+            if reddit_data.get(date) is None:
+                reddit_data[date] = {}
+            reddit_data[date][text] = item['doc_count']
 
-    cache = []
-
-    for c in gen:
-        cache.append(c)
-    row = 0
-    worksheet.write(row,col,text)
-    for x in cache[0]['created_utc']:
-        row = row + 1
-        worksheet.write(row,col,x['doc_count'])
-
-workbook.close()
+reddit_df = df.from_dict(reddit_data,orient='index')
+reddit_df.to_csv("reddit.csv",index=True, header=True)   
