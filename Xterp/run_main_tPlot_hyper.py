dd11f64a6f991472d9ee1a2a9b499a68a5a84065
jacksonflats <dshribak@rocketmail.com>
2020-04-18 12:03:49 -0400

    Comments Gen 4

--- Xterp/run_main_tPlot_hyper.py
+++ Xterp/run_main_tPlot_hyper.py
@@ -184,6 +184,40 @@ def plot_future(predicted_data, true_data, prediction_len, ticker, isTrends, fil
     plt.show()
 
 
+def predict_point_by_point(model, data):
+    # Predict each timestep given the last sequence of true data, in effect only predicting 1 step ahead each time
+    print('[Model] Predicting Point-by-Point...')
+    predicted = model.predict(data)
+    predicted = np.reshape(predicted, (predicted.size,))
+    return predicted
+
+
+def predict_sequences_multiple(model, data, window_size, prediction_len):
+    # Predict sequence of 50 steps before shifting prediction run forward by 50 steps
+    print('[Model] Predicting Sequences Multiple...')
+    prediction_seqs = []
+    for i in range(int(len(data) / prediction_len)):
+        curr_frame = data[i * prediction_len]
+        predicted = []
+        for j in range(prediction_len):
+            predicted.append(model.predict(curr_frame[newaxis, :, :])[0, 0])
+            curr_frame = curr_frame[1:]
+            curr_frame = np.insert(curr_frame, [window_size - 2], predicted[-1], axis=0)
+        prediction_seqs.append(predicted)
+    return prediction_seqs
+
+
+def predict_sequence_full(model, data, window_size):
+    # Shift the window by 1 new prediction each time, re-run predictions on new window
+    print('[Model] Predicting Sequences Full...')
+    curr_frame = data[0]
+    predicted = []
+    for i in range(len(data)):
+        predicted.append(model.predict(curr_frame[newaxis, :, :])[0, 0])
+        curr_frame = curr_frame[1:]
+        curr_frame = np.insert(curr_frame, [window_size - 2], predicted[-1], axis=0)
+    return predicted
+
 
 def main():
 
