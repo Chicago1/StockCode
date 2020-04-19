d842e5d49e65525a7da12abae3dacb5c9eb67fa3
jacksonflats <dshribak@rocketmail.com>
2020-04-18 21:58:36 -0400

    Comments gen 5

--- Xterp/run_main_tPlot_noTrain.py
+++ Xterp/run_main_tPlot_noTrain.py
@@ -25,10 +25,14 @@ import time
 from core.data_processor import DataLoader #no error, from the core folder
 from core.model import Model
 
+
+from tensorflow.keras.models import Sequential, load_model
+
+
 os.environ['KMP_DUPLICATE_LIB_OK']='True' #setting that lets tensorflow run
 
 #below not really used, seems to be for single prediction (not multiple)
-def plot_results(predicted_data, true_data,prediction_len, ticker, isTrends ,filename, split):
+def plot_results(predicted_data, true_data, prediction_len, ticker, isTrends, filename, split):
     fig = plt.figure(facecolor='white')
     ax = fig.add_subplot(111)
 
@@ -36,13 +40,12 @@ def plot_results(predicted_data, true_data,prediction_len, ticker, isTrends ,fil
 
     i_split = int(len(dataframe) * split) + prediction_len
 
-
     dates = mdates.date2num(pd.to_datetime(dataframe.iloc[i_split:len(dataframe), 0]))
 
-    plt.plot_date(dates, np.transpose(np.array(predicted_data)), label='Prediction',fmt="-")
+    plt.plot_date(dates, np.transpose(np.array(predicted_data)), label='Prediction', fmt="-")
 
-    months = mdates.MonthLocator()  # every month
-    months_fmt = mdates.DateFormatter('%Y-%m')
+    months = mdates.WeekdayLocator()  # every month
+    months_fmt = mdates.DateFormatter('%Y-%m-%d')
 
     plt.plot_date(dates, true_data, fmt="-", color="cornflowerblue", linewidth="0.5")
 
@@ -117,6 +120,101 @@ def plot_results_multiple(predicted_data, true_data, prediction_len, ticker, isT
 
     plt.show()
 
+
+def plot_training(predicted_data, true_data, prediction_len, ticker, isTrends, filename, split):
+    '''
+    Plots results from training
+    '''
+
+    fig = plt.figure(facecolor='white', figsize=(6.4*7, 5))
+
+    ax = fig.add_subplot(111)
+
+
+    dataframe = pd.read_csv(filename)
+    i_split = int(len(dataframe) * split)
+
+    dates = mdates.date2num(pd.to_datetime(dataframe.iloc[0:i_split-prediction_len, 0]))
+
+    plt.plot_date(dates, predicted_data, color= 'red', label='Prediction', fmt="-")
+
+
+    months = mdates.MonthLocator()  # every month
+    months_fmt = mdates.DateFormatter('%Y-%m')
+
+    plt.plot_date(dates, true_data, fmt="-", color="cornflowerblue", linewidth="0.5")
+
+    ax.xaxis.set_major_locator(months)
+    ax.xaxis.set_major_formatter(months_fmt)
+
+    # format the coords message box
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    ax.grid(True)
+
+    fig.autofmt_xdate()
+
+
+    if isTrends:
+        plt.title(str(ticker)+" with Trends")
+        plt.savefig(str(ticker)+"_with_Trends.png")
+        print("saved")
+    else:
+        plt.title(str(ticker)+" without Trends")
+        plt.savefig(str(ticker)+"_without_Trends.png")
+        print("saved")
+
+
+    plt.show()
+
+def plot_future(predicted_data, true_data, prediction_len, ticker, isTrends, filename, split):
+    '''
+    Plots results from training
+    '''
+
+    fig = plt.figure(facecolor='white')
+
+    ax = fig.add_subplot(111)
+
+
+    dataframe = pd.read_csv(filename)
+    i_split = int(len(dataframe) * split)
+
+    dates = mdates.date2num(pd.to_datetime(dataframe.iloc[0:i_split-prediction_len, 0]))
+
+    plt.plot_date(dates, predicted_data, color= 'red', label='Prediction', fmt="-")
+
+
+    months = mdates.MonthLocator()  # every month
+    months_fmt = mdates.DateFormatter('%Y-%m')
+
+    plt.plot_date(dates, true_data, fmt="-", color="cornflowerblue", linewidth="0.5")
+
+    ax.xaxis.set_major_locator(months)
+    ax.xaxis.set_major_formatter(months_fmt)
+
+    # ax.plot((np.array(dataframe.iloc[i_split:len(dataframe), 0])),true_data)
+
+    # format the coords message box
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    ax.grid(True)
+
+    fig.autofmt_xdate()
+
+
+    if isTrends:
+        plt.title(str(ticker)+" with Trends")
+        plt.savefig(str(ticker)+"_with_Trends.png")
+        print("saved")
+    else:
+        plt.title(str(ticker)+" without Trends")
+        plt.savefig(str(ticker)+"_without_Trends.png")
+        print("saved")
+
+
+    plt.show()
+
+
+
 def main():
 
     #MAKE SURE BOTH DATASETS (yahoo stock and google trends) EXACT SAME LENGTH AND FILLED
@@ -139,52 +237,36 @@ def main():
     #Creates model save directory
     if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
 
-    #Creates a new Model object, see core/model to see what it does.
     model = Model()
-    model.build_model(configs)
-    x, y = data.get_train_data(
-        seq_len=configs['data']['sequence_length'],
-        normalise=configs['data']['normalise']
-    )
+    model.load_model('saved_models/18042020-210250-e200.h5')
 
-    # # out-of memory generative training
-    # steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
-    # model.train_generator(
-    #     data_gen=data.generate_train_batch(
-    #         seq_len=configs['data']['sequence_length'],
-    #         batch_size=configs['training']['batch_size'],
-    #         normalise=configs['data']['normalise']
-    #     ),
-    #     epochs=configs['training']['epochs'],
-    #     batch_size=configs['training']['batch_size'],
-    #     steps_per_epoch=steps_per_epoch,
-    #     save_dir=configs['model']['save_dir']
-    # )
-
-    # in-memory training
-    model.train(x, y, epochs=configs['training']['epochs'], batch_size=configs['training']['batch_size'],
-                save_dir=configs['model']['save_dir'])
 
     x_test, y_test = data.get_test_data(
         seq_len=configs['data']['sequence_length'],
         normalise=configs['data']['normalise']
     )
 
-    print("x_test")
-    print(x_test)
-    print("-----")
-    print("y_test")
-    print(y_test)
-
-    predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
+    predictions = model.predict_point_by_point(x_test)
 
 
     # predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
     # predictions = model.predict_point_by_point(x_test)
-     
-    stockTicker = "VOX"
-    plot_results_multiple(predictions, y_test, configs['data']['sequence_length'], stockTicker, True, configs['data']['filename'], configs['data']['train_test_split'])
+
+
+    stockTicker = "VNQ"
     # plot_results(predictions, y_test)
+
+    plot_results(predictions, y_test,configs['data']['sequence_length'], stockTicker, True, configs['data']['filename'], configs['data']['train_test_split'])
+
+
+
+    sys.exit(99)
+
+    x_train, y_train = data.get_train_data(seq_len=configs['data']['sequence_length'], normalise=configs['data']['normalise'])
+
+    train_predictions = model.predict_point_by_point(x_train)
+
+    plot_training(train_predictions, y_train, configs['data']['sequence_length'], stockTicker, True, configs['data']['filename'],  configs['data']['train_test_split'])
 
 
 if __name__ == '__main__':
